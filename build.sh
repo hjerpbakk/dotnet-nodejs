62d0e44c7a5010319097c94f8adcac2ef97857d6
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2021-09-21 22:16:01 +0200

    Added build and publish scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build -t hjerpbakk/dotnet-nodejs:latest .
+docker build -t hjerpbakk/dotnet-nodejs:5.0-sdk-node-16.x .
---
+++ publish.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker push hjerpbakk/dotnet-nodejs:latest
+docker push hjerpbakk/dotnet-nodejs:5.0-sdk-node-16.x
